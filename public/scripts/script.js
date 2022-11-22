f2af515a29c82ac3055f7c53e3a405a13e4ebbcc
Marcomussa <marcomussa567@gmail.com>
2022-11-22 12:11:09 -0300

    cambios

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,11 +1,23 @@
+let mainSectionImg_2 = document.querySelector(".mainSectionImg_2")
+let mainSectionImg_3 = document.querySelector(".mainSectionImg_3")
+
 window.addEventListener("scroll", () => {
 
     const { scrollTop, clientHeight } = document.documentElement
 
     console.log(scrollTop, clientHeight)
 
-    if(scrollTop == 500){
-        console.log(true)
+    for(let i = 600; i <= 660; i++){
+        if(scrollTop == i){
+            console.log(mainSectionImg_3)
+            mainSectionImg_3.classList.add("animate__backInRight")
+        }
     }
 
+    for(let i = 185; i <= 230; i++){
+        if(scrollTop == i){
+            console.log(mainSectionImg_2)
+            mainSectionImg_2.classList.add("animate__backInLeft")
+        }
+    }
 })
